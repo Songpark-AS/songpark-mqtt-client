fd8b637ed512a6ddc4548a37dc9171cfc10c32fb
Emil Bengtsson <emil@emil0r.com>
2022-02-21 15:52:21 +0100

    Add new topics
    Add topics for a specific app and teleporters.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject songpark/mqtt "1.0.0-alpha4"
+(defproject songpark/mqtt "1.0.0-alpha5"
   :description "MQTT library for Songpark"
 
   :dependencies [;; clojure
--- src/songpark/mqtt/util.cljc
+++ src/songpark/mqtt/util.cljc
@@ -2,3 +2,9 @@
 
 (defn broadcast-topic [id]
   (str id "/broadcast"))
+
+(defn app-topic [id]
+  (str id "/app"))
+
+(defn teleporter-topic [id]
+  (str id "/teleporter"))
